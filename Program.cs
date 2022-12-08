6dab896f44ab538ff019aa90fba72954c6f373d1
Johnny <johnstuartgonsales@gmail.com>
2022-12-08 06:06:57 +0000

    asks user for number of bills rather than constantly asking if there is another

--- Program.cs
+++ Program.cs
@@ -11,40 +11,27 @@ class Bills
     static void Main()
     {
         double starting_money = 512.71;
-        Console.WriteLine("Do you have any bills?");
-        string userResponse = Console.ReadLine();
-        if (userResponse == "yes")
+        Console.WriteLine("How many bills do you have?");
+        int userResponse = (Convert.ToInt32(Console.ReadLine()));
+        var bills = new List<double>();
+        for (int i = 0; i < userResponse; i++)
         {
-            var bills = new List<double>();
-            while (true)
-            {
-                Console.WriteLine("Bill amount:");
-                bills.Add(Convert.ToDouble(Console.ReadLine()));
-                Console.WriteLine("Do you have another bill?");
-                if (Console.ReadLine().ToLower() != "yes")
-                {
-                    break;
-                }
-            }  
-        
-            Console.WriteLine("Will you be saving anything this month?");
-            string save_answer = (Console.ReadLine());
-            double savings = 0;
-            if (save_answer.ToLower() == "yes")
-            {
-                Console.WriteLine("How much are you saving this month?");
-                savings = (Convert.ToInt32(Console.ReadLine()));
-            }
-            double bill_total = bills.Sum();
-            double final_total = starting_money - bill_total - savings;
-
-            Console.WriteLine($"Your bills come to £{Math.Round(bill_total, 2)}");
-            Console.WriteLine($"Your savings come to £{Math.Round(savings, 2)}");
-            Console.WriteLine($"Your spending money comes to £{Math.Round(final_total, 2)}");
-        } 
-        else 
-        {
-            Console.WriteLine("What do you mean you don't have bills?");
+            Console.WriteLine($"Bill #{i + 1}: ");
+            bills.Add(Convert.ToDouble(Console.ReadLine()));
         }
-    }
+        Console.WriteLine("Will you be saving anything this month?");
+        string save_answer = (Console.ReadLine());
+        double savings = 0;
+        if (save_answer.ToLower() == "y")
+        {
+            Console.WriteLine("How much are you saving this month?");
+            savings = (Convert.ToInt32(Console.ReadLine()));
+        }
+        double bill_total = bills.Sum();
+        double final_total = starting_money - bill_total - savings;
+
+        Console.WriteLine($"Your bills come to £{Math.Round(bill_total, 2)}");
+        Console.WriteLine($"Your savings come to £{Math.Round(savings, 2)}");
+        Console.WriteLine($"Your spending money comes to £{Math.Round(final_total, 2)}");
+    } 
 }
